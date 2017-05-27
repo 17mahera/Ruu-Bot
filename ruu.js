6e089a4b8005676af6a3bdc931125bfdad808996
17mahera <maomaoam.aki@gmail.com>
2017-05-26 20:44:24 -0400

    Lotsa commands added, config file added
    made for easy editing and testing command functionality

--- ruu.js
+++ ruu.js
@@ -1,5 +1,6 @@
 // Requires
 const Discord = require('discord.js')
+const fs = require('fs')
 
 // Create an instance of a Discord client
 const client = new Discord.Client()
@@ -9,11 +10,52 @@ client.on('ready', () => {
   console.log('Bot Started')
 })
 
-
 client.login(process.env.BOT_TOKEN)
 
 bot.on('message', message => { // when message is sent
     if (message.content ===("<>")){ // when message is !bot
         message.channel.sendMessage("Shinies!"); // send running message into the channel where the message was sent
     }
+});
+client.on('message', (message) => {
+  // Set the prefix
+  let prefix = config.prefix;
+
+  // Exit and stop if it's not there
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(prefix + 'help'))
+  {
+    message.channel.send('I don\'t know what to help with yet!');
+  }
+  else if (message.content.startsWith(prefix + 'idoruu'))
+  {
+    message.channel.send('http://i.imgur.com/Emk9LEb.png!');
+  }
+
+  else if(message.content.startsWith(config.prefix + 'prefix'))
+  {
+    // if it's not me it won't work
+    if(message.author.id !== config.ownerID) return;
+  // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
+  let newPrefix = message.content.split(' ').slice(1, 2)[0];
+  // change the configuration in memory
+  config.prefix = newPrefix;
+
+  // Now we have to save the file.
+  fs.writeFile('./config.json', JSON.stringify(config), (err) => if(err) console.error(err));
+  }
+  else if(message.content.startsWith(config.prefix + 'waifu'))
+  {
+    // if it's not me it won't work
+    if(message.author.id !== config.ownerID)
+    {
+      message.channel.send('Not AA-senpai go away');
+    }
+    else
+    {
+    message.channel.send('AA-senpai!');
+    }
+
+  }
 });
